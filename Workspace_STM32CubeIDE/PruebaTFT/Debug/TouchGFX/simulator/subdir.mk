26d4ea93e18d1f118eb1b4ff561a9e35ca52ce61
AleNimo <aledn2000@gmail.com>
2022-12-01 00:20:36 -0300

    Update Gitignore

---
+++ Workspace_STM32CubeIDE/PruebaTFT/Debug/TouchGFX/simulator/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../TouchGFX/simulator/main.cpp 
+
+OBJS += \
+./TouchGFX/simulator/main.o 
+
+CPP_DEPS += \
+./TouchGFX/simulator/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+TouchGFX/simulator/%.o TouchGFX/simulator/%.su: ../TouchGFX/simulator/%.cpp TouchGFX/simulator/subdir.mk
+	arm-none-eabi-g++ "$<" -mcpu=cortex-m4 -std=gnu++14 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F401xE -c -I../Core/Inc -I../TouchGFX/App -I../TouchGFX/target/generated -I../TouchGFX/target -I../USB_DEVICE/App -I../USB_DEVICE/Target -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I../Middlewares/ST/STM32_USB_Device_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Device_Library/Class/CustomHID/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti -fno-use-cxa-atexit -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-TouchGFX-2f-simulator
+
+clean-TouchGFX-2f-simulator:
+	-$(RM) ./TouchGFX/simulator/main.d ./TouchGFX/simulator/main.o ./TouchGFX/simulator/main.su
+
+.PHONY: clean-TouchGFX-2f-simulator
+
